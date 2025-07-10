c3d81108623664139d9c2cba2900caee9019db9d
崔海兴 <3203809808@qq.com>
2025-07-11 00:09:00 +0800

    功能都正常

--- user-service/src/main/java/ynu/edu/user_service/controller/UserController.java
+++ user-service/src/main/java/ynu/edu/user_service/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ynu.edu.common.utils.JwtUtil;
 import ynu.edu.user_service.dto.ErrorResponseCustomer;
 import ynu.edu.user_service.dto.LoginResponse;
 import ynu.edu.user_service.po.User;
@@ -21,21 +20,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private JwtUtil jwtUtil;
+
 
     @PostMapping("/getUserByIdByPass")
-    public ResponseEntity<?> getUserByIdByPass(@RequestBody User user) {
-        User authenticatedUser = userService.getUserByIdByPass(user);
-        if (authenticatedUser != null) {
-            // 生成Token，这里使用用户ID作为subject
-            String token = jwtUtil.generateToken(authenticatedUser.getUserId());
-            return ResponseEntity.ok()
-                    .body(new LoginResponse("登录成功", token));
-        } else {
-            return ResponseEntity.status(401)
-                    .body(new ErrorResponseCustomer("用户名或密码错误"));
-        }
+    public User getUserByIdByPass(@RequestBody User user) {
+        return userService.getUserByIdByPass(user);
+
     }
 
     @GetMapping("/getUserById/{userId}")
