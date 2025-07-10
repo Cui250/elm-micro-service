77a16d63d5d70925de8ea72c58260734b25ccc1c
崔海兴 <3203809808@qq.com>
2025-07-11 00:13:14 +0800

    功能都正常

--- api-gateway/src/main/java/ynu/edu/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/ynu/edu/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,8 @@ package ynu.edu.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-        "ynu.edu.common.utils" // 添加公共模块的扫描路径
-})public class ApiGatewayApplication {
+@SpringBootApplication
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
